caf205656453cb01802cc3e55f0ddec1bd60c5db
Erdem Acar <119631273+erdemacar6@users.noreply.github.com>
2023-02-19 13:09:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,15 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        Girilen bir sayının armstrong sayısını olup olmadığını bulmaya çalışın.
-        Örneğin, 4 basamaklı bir sayının armstrong sayısı olması için şu şartı sağlaması gerekmektedir.
-        1634 = 1^4 + 6^4 + 3^4 + 4^4 (Buradaki 4 basamak sayısı)
-        371 = 3^3 + 7^3 + 1^3 (Buradaki 3 basamak sayısı)
-
-        Bu programı do while döngüsü yardımıyla yapabilirsiniz.
-        */
-
+     
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen bir sayı giriniz : ");
         int sayi = scanner.nextInt();
